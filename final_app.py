03b6a37339ae5575f49c16f4b752786c9c2d38b8
Battlepenny2122 <121442061+Battlepenny2122@users.noreply.github.com>
2023-05-15 22:07:07 +1000

    Update final_app.py

--- final_app.py
+++ final_app.py
@@ -9,7 +9,7 @@ This website queries weatherapi.com! Please enter your city and we'll get you yo
 city = st.text_input('Which city do you live in?', value='Sydney')
 api_key = '2fb4fc4ca9994f03bbc234017231405'
 
-if st.button('Submit'):
+if st.button('Submit') or city:
     # enter here the address of your flask api
     url = 'http://api.weatherapi.com/v1/forecast.json'
     params = dict(
