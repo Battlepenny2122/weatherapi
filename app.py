535506e806ef2db18d651c315b185ed638c34e86
Battlepenny2122 <121442061+Battlepenny2122@users.noreply.github.com>
2023-05-15 19:25:24 +1000

    Update app.py

--- app.py
+++ app.py
@@ -31,32 +31,45 @@ if st.button('Submit'):
     pred = prediction['current']['temp_c']
 
 # redo this with Class Object next time! DRY
-    pred1max = prediction['forecast']['forecastday'][0]['day']['maxtemp_c']
-    pred1min = prediction['forecast']['forecastday'][0]['day']['mintemp_c']
-    image_icon1 = prediction['forecast']['forecastday'][0]['day']['condition']['icon']
-    condition1 = prediction['forecast']['forecastday'][0]['day']['condition']['text']
 
-    pred2max = prediction['forecast']['forecastday'][1]['day']['maxtemp_c']
-    pred2min = prediction['forecast']['forecastday'][1]['day']['mintemp_c']
-    image_icon2 = prediction['forecast']['forecastday'][1]['day']['condition']['icon']
-    condition2 = prediction['forecast']['forecastday'][1]['day']['condition']['text']
+predmax_list = []
+predmin_list = []
+image_icon_list = []
+condition_list = []
 
-    pred3max = prediction['forecast']['forecastday'][2]['day']['maxtemp_c']
-    pred3min = prediction['forecast']['forecastday'][2]['day']['mintemp_c']
-    image_icon3 = prediction['forecast']['forecastday'][2]['day']['condition']['icon']
-    condition3 = prediction['forecast']['forecastday'][2]['day']['condition']['text']
+for i in range(0,5):
+    predmax_list.append(prediction['forecast']['forecastday'][i]['day']['maxtemp_c'])
+    predmin_list.append(prediction['forecast']['forecastday'][i]['day']['mintemp_c'])
+    image_icon_list.append(prediction['forecast']['forecastday'][i]['day']['condition']['icon'])
+    condition_list.append(prediction['forecast']['forecastday'][i]['day']['condition']['text'])
 
 
-    pred4max = prediction['forecast']['forecastday'][3]['day']['maxtemp_c']
-    pred4min = prediction['forecast']['forecastday'][3]['day']['mintemp_c']
-    image_icon4 = prediction['forecast']['forecastday'][3]['day']['condition']['icon']
-    condition4 = prediction['forecast']['forecastday'][3]['day']['condition']['text']
+    # pred1max = prediction['forecast']['forecastday'][0]['day']['maxtemp_c']
+    # pred1min = prediction['forecast']['forecastday'][0]['day']['mintemp_c']
+    # image_icon1 = prediction['forecast']['forecastday'][0]['day']['condition']['icon']
+    # condition1 = prediction['forecast']['forecastday'][0]['day']['condition']['text']
+
+    # pred2max = prediction['forecast']['forecastday'][1]['day']['maxtemp_c']
+    # pred2min = prediction['forecast']['forecastday'][1]['day']['mintemp_c']
+    # image_icon2 = prediction['forecast']['forecastday'][1]['day']['condition']['icon']
+    # condition2 = prediction['forecast']['forecastday'][1]['day']['condition']['text']
+
+    # pred3max = prediction['forecast']['forecastday'][2]['day']['maxtemp_c']
+    # pred3min = prediction['forecast']['forecastday'][2]['day']['mintemp_c']
+    # image_icon3 = prediction['forecast']['forecastday'][2]['day']['condition']['icon']
+    # condition3 = prediction['forecast']['forecastday'][2]['day']['condition']['text']
 
 
-    pred5max = prediction['forecast']['forecastday'][4]['day']['maxtemp_c']
-    pred5min = prediction['forecast']['forecastday'][4]['day']['mintemp_c']
-    image_icon5 = prediction['forecast']['forecastday'][4]['day']['condition']['icon']
-    condition5 = prediction['forecast']['forecastday'][4]['day']['condition']['text']
+    # pred4max = prediction['forecast']['forecastday'][3]['day']['maxtemp_c']
+    # pred4min = prediction['forecast']['forecastday'][3]['day']['mintemp_c']
+    # image_icon4 = prediction['forecast']['forecastday'][3]['day']['condition']['icon']
+    # condition4 = prediction['forecast']['forecastday'][3]['day']['condition']['text']
+
+
+    # pred5max = prediction['forecast']['forecastday'][4]['day']['maxtemp_c']
+    # pred5min = prediction['forecast']['forecastday'][4]['day']['mintemp_c']
+    # image_icon5 = prediction['forecast']['forecastday'][4]['day']['condition']['icon']
+    # condition5 = prediction['forecast']['forecastday'][4]['day']['condition']['text']
 
     city2 = prediction['location']['name']
     country = prediction['location']['country']
@@ -70,54 +83,73 @@ if st.button('Submit'):
         st.write(f'The **minimum** temperature today is {pred1min}{chr(176)}C')
 
         st.markdown('**Forecast for the week**')
-
+    
     today_date = datetime.today()
-    tomorrow_date = today_date + timedelta(days=1)
-    two_day = today_date + timedelta(days=2)
-    three_day = today_date + timedelta(days=3)
-    four_day = today_date + timedelta(days=4)
+    dates = [today_date]
 
-    today = today_date.strftime('%A')
-    tomorrow = tomorrow_date.strftime('%A')
-    two = two_day.strftime('%A')
-    three = three_day.strftime('%A')
-    four = four_day.strftime('%A')
+    for i in range(1,5):
+        date = today_date + timedelta(days = i)
+        dates.append(date.strftime('%A'))
 
-    col1,col2,col3,col4,col5 = st.columns(5)
+
+    # today_date = datetime.today()
+    # tomorrow_date = today_date + timedelta(days=1)
+    # two_day = today_date + timedelta(days=2)
+    # three_day = today_date + timedelta(days=3)
+    # four_day = today_date + timedelta(days=4)
+
+    # today = today_date.strftime('%A')
+    # tomorrow = tomorrow_date.strftime('%A')
+    # two = two_day.strftime('%A')
+    # three = three_day.strftime('%A')
+    # four = four_day.strftime('%A')
+
+    col = st.columns(5)
+
+    for i in range(col):
+        with col[i]:
+            st.write(f'{day[i]}')
+            st.image('https:' + image_icon_list[i])
+            st.write(f'{condition_list[i]}')
+            st.write(f'Max temp: {predmax_list[i]}{chr(176)}C')
+            st.write(f'Min temp: {predmin_list[i]}{chr(176)}C')
+
+
+    # col1,col2,col3,col4,col5 = st.columns(5)
     
-    with col1:
-        st.write(f'{today}')
-        st.image('https:' + image_icon1)
-        st.write(f'{condition1}')
-        st.write(f'Max temp: {pred1max}{chr(176)}C')
-        st.write(f'Min temp: {pred1min}{chr(176)}C')
-    with col2:
-        st.write(f'{tomorrow}')
-        st.image('https:' + image_icon2)
-        st.write(f'{condition2}')
-        st.write(f'Max temp: {pred2max}{chr(176)}C')
-        st.write(f'Min temp: {pred2min}{chr(176)}C')  
+    # with col1:
+    #     st.write(f'{today}')
+    #     st.image('https:' + image_icon1)
+    #     st.write(f'{condition1}')
+    #     st.write(f'Max temp: {pred1max}{chr(176)}C')
+    #     st.write(f'Min temp: {pred1min}{chr(176)}C')
+    # with col2:
+    #     st.write(f'{tomorrow}')
+    #     st.image('https:' + image_icon2)
+    #     st.write(f'{condition2}')
+    #     st.write(f'Max temp: {pred2max}{chr(176)}C')
+    #     st.write(f'Min temp: {pred2min}{chr(176)}C')  
 
-    with col3:
-        st.write(f'{two}')
-        st.image('https:' + image_icon3)
-        st.write(f'{condition3}')
-        st.write(f'Max temp: {pred3max}{chr(176)}C')
-        st.write(f'Min temp: {pred3min}{chr(176)}C')
+    # with col3:
+    #     st.write(f'{two}')
+    #     st.image('https:' + image_icon3)
+    #     st.write(f'{condition3}')
+    #     st.write(f'Max temp: {pred3max}{chr(176)}C')
+    #     st.write(f'Min temp: {pred3min}{chr(176)}C')
     
-    with col4:
-        st.write(f'{three}')
-        st.image('https:' + image_icon4)
-        st.write(f'{condition4}')
-        st.write(f'Max temp: {pred4max}{chr(176)}C')
-        st.write(f'Min temp: {pred4min}{chr(176)}C')
+    # with col4:
+    #     st.write(f'{three}')
+    #     st.image('https:' + image_icon4)
+    #     st.write(f'{condition4}')
+    #     st.write(f'Max temp: {pred4max}{chr(176)}C')
+    #     st.write(f'Min temp: {pred4min}{chr(176)}C')
 
-    with col5:
-        st.write(f'{four}')
-        st.image('https:' + image_icon5)
-        st.write(f'{condition5}')
-        st.write(f'Max temp: {pred5max}{chr(176)}C')
-        st.write(f'Min temp: {pred5min}{chr(176)}C')
+    # with col5:
+    #     st.write(f'{four}')
+    #     st.image('https:' + image_icon5)
+    #     st.write(f'{condition5}')
+    #     st.write(f'Max temp: {pred5max}{chr(176)}C')
+    #     st.write(f'Min temp: {pred5min}{chr(176)}C')
         
 # st.markdown('I used weatherapi.com, and I am using st.markdown to test **_different fonts_**')
 # st.markdown('Bayes Theorem is :green[P(A|B) = P(B|A)*P(A)/P(B)]')
